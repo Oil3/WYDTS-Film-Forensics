4ff54e2375eca76a813f27eff8ee9ad03a89faa0
oil3 <3oiltree@gmail.com>
2024-07-20 05:11:48 -0500

    Initial Commit

---
+++ WYDTS Forensics/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WYDTS Forensics
+//
+//  Created by Almahdi Morris on 20/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WYDTS Forensics/WYDTS_ForensicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WYDTS_ForensicsApp.swift
+//  WYDTS Forensics
+//
+//  Created by Almahdi Morris on 20/7/24.
+//
+
+import SwiftUI
+
+@main
+struct WYDTS_ForensicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
